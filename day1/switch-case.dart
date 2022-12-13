0c3f28b1aa4c52b51bd3eb81c739d6af7e335f54
yaw.o.k <obengky72@gmail.com>
2022-12-13 09:40:18 +0000

    Add switch statement examples

---
+++ day1/switch-case.dart
@@ -0,0 +1,31 @@
+// author: Kenneth Yaw Obeng(yawok)
+// examples of switch and case statements
+
+void main() {
+  var firstName = "Winfred";
+  String? lastName;
+  switch (firstName) {
+    case "Keneth":
+      lastName = "Obeng";
+      break;
+    case "Winfred":
+      lastName = "Adu-Acheampong";
+      break;
+    default:
+      print("First name has no match in the database");
+  }
+
+  switch (10 - 4){
+    case 4:
+      print("The answer is 4")
+      break;
+    case 5:
+      print("The answer is 5");
+      break;
+    default:
+      print("The answer is not 4 or 5");
+  }
+  
+  }
+
+}
