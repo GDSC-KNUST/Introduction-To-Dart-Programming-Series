475905d4aac8b196251957fc6d35dbdababc52eb
yaw.o.k <obengky72@gmail.com>
2022-12-13 08:32:33 +0000

    Add raw strings example

--- day1/strings.dart
+++ day1/strings.dart
@@ -5,6 +5,16 @@ void main() {
   // string declarations
   var name = 'Kenneth';
   String city = "Kumasi";
+  String notRawString = 'Randy\n John\n Stephen';
+
+  
+  // raw strings
+  var aRawString = r'Who lives in a \npineapple under the sea?';
+  String exactUserInput = r'Randy\n John\n Stephen';
+  print(notRawString);
+  print(exactUserInput);
+
+
   
   // string constants
   final fileName = ' strings.dart  ';
