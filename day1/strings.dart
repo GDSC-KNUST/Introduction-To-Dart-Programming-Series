74e261ee1e348bdd2781f2dba4a3dc339bd75d6f
yaw.o.k <obengky72@gmail.com>
2022-12-13 08:18:14 +0000

    Add file on string datatype

---
+++ day1/strings.dart
@@ -0,0 +1,19 @@
+// author: Kenneth Yaw Obeng(yawok)
+// string declarations examples
+
+void main() {
+  // string declarations
+  var name = 'Kenneth';
+  String city = "Kumasi";
+  
+  // string constants
+  final fileName = ' strings.dart  ';
+  const fileType = '.dart';
+
+  // some string methods and properties
+  print(name.length);
+  print(name.toUpperCase());
+  print(city.toLowerCase());
+  print(fileName.trim());
+
+}
